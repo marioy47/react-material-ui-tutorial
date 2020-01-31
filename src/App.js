9dd8143c5c6d0292fa06ec9d6db22b51314e9853
Mario Yepes <marioy47@gmail.com>
2020-01-31 09:06:08 -0500

    Create a theme file and apply it to the App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 // src/App.js
 import React from 'react';
+import { Button } from '@material-ui/core'
 
 function App() {
   return (
     <div className="App">
       <h1>React material Tutorial</h1>
+      <h2>This Could be a subtitle</h2>
+      <p>And this is a paragraph</p>
+      <Button variant="contained" color="primary">
+        A Primary button
+      </Button>
+      <Button variant="contained" color="secondary">
+        A Secondary button
+      </Button>
     </div>
   );
 }
---
+++ src/theme.js
@@ -0,0 +1,18 @@
+// src/theme.js
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#00695c'
+    },
+    secondary: {
+      main: '#c0ca33'
+    },
+    background: {
+      default: '#fff'
+    }
+  }
+});
+
+export default theme;
